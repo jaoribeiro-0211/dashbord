6dad6e5ab5556136867494589db99eac1ed5518a
João Vitor Ribeiro <joaotvrodeio@gmail.com>
2022-05-04 12:25:57 -0300

    Subindo projeto

---
+++ src/components/ModalFactory/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <teleport to="body">
+    <div
+      class="fixed top-0 left-0 z-50 flex items-center justify-center w-full bg-black bg-opacity-50  h-hull"
+    >
+      <div class="fixed mx-10">
+        <div
+          class="flex flex-col overflow-hidden bg-white rounded-lg  animate__animated animate__fadeInDown animate__faster"
+        >
+          <div class="flex flex-col px-12 py-10 bg-white">
+            <component :is="state.component" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  setup() {
+    const state = reactive({
+      isActive: false,
+      component: {},
+      props: {},
+      width: DEFAULT_WIDTH,
+    });
+
+    return {
+      state,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,41 @@
 <template>
-  <div class="text-brand-main font-black">Home</div>
+  <CustomHeader />
+  <Contact />
+  <div class="flex justify-center py-10">
+    <p class="font-medium text-center text-gray-800">Feedbacker 2021</p>
+  </div>
 </template>
 
 <script>
+import CustomHeader from "./CustomHeader.vue";
+import Contact from "./Contact.vue";
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Home",
+  components: {
+    CustomHeader,
+    Contact,
+  },
+  setup() {
+    const router = useRouter("token");
+
+    onMounted(() => {
+      const token = window.localStorage.getItem("token");
+      if (token) {
+        router.push({ name: "Feedbacks" });
+      }
+    });
+
+    function handleLogin() {}
+    function handleAccountCreate() {}
+
+    return {
+      handleLogin,
+      handleAccountCreate,
+    };
+  },
 };
 </script>
 
